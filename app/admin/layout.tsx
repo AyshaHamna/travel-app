14522c391dfac0ee63221290b4baf2bff98deb75
AyshaHamna <ayshahamna57@gmail.com>
2024-08-11 15:05:46 +0530

    dashboard added

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,23 +1,24 @@
-import AdminHeader from "@/components/AdminHeader";
-import Sidebar from "@/components/Sidebar";
+"use client";
+
+import AdminSidebar from "@/components/AdminSidebar";
 import { ClerkLoaded } from "@clerk/nextjs";
+import { useState } from "react";
 
 function AdminLayout({ children }: { children: React.ReactNode }) {
+  const [sidebarOpen, setSidebarOpen] = useState(true);
   return (
     <ClerkLoaded>
-      {/* <div className="flex flex-col h-screen bg-gray-100 dark:bg-gray-900 md:flex-row">
-        <div className="order-2"><Sidebar/></div>
-        <div className="flex-1 flex flex-col">
-          <AdminHeader />
-          <main className="flex-1 p-4 md:p-6 md:m-2 overflow-y-auto">{children}</main>
-        </div>
-      </div> */}
-      <div className="flex flex-col h-screen bg-gray-100 dark:bg-gray-900 ">
-        <div className="flex flex-row">
-          <Sidebar />
-          <AdminHeader />
-        </div>
-        <main>{children}</main>
+      <div className="flex-1 flex flex-col h-screen w-full bg-gray-100 dark:bg-neutral-800 overflow-hidden md:flex-row">
+        <AdminSidebar
+          sidebarOpen={sidebarOpen}
+          setSidebarOpen={setSidebarOpen}
+        />
+        {/* dashboard main content */}
+        <main className="flex-1 flex">
+          <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full">
+            {children}
+          </div>
+        </main>
       </div>
     </ClerkLoaded>
   );
---
+++ components/AdminSidebar.tsx
@@ -0,0 +1,115 @@
+import { motion } from "framer-motion";
+import { link } from "fs";
+import {
+  BookCheck,
+  CarFront,
+  LayoutDashboard,
+  MapPin,
+  TentTree,
+} from "lucide-react";
+import Link from "next/link";
+import { Dispatch, ReactNode, SetStateAction } from "react";
+import { Sidebar, SidebarBody, SidebarLink } from "./ui/sidebar";
+import Image from "next/image";
+import seaImage from "/Images/sea.jpg";
+
+interface AdminSidebarProps {
+  sidebarOpen: boolean;
+  setSidebarOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+function AdminSidebar({ sidebarOpen, setSidebarOpen }: AdminSidebarProps) {
+  const links = [
+    {
+      label: "Dashboard",
+      href: "#",
+      icon: (
+        <LayoutDashboard className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+      ),
+    },
+    {
+      label: "Tours",
+      href: "admin/tour",
+      icon: (
+        <CarFront className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+      ),
+    },
+    {
+      label: "Bookings",
+      href: "#",
+      icon: (
+        <BookCheck className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+      ),
+    },
+    {
+      label: "Popular Destinations",
+      href: "#",
+      icon: (
+        <MapPin className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+      ),
+    },
+  ];
+
+  return (
+    <Sidebar open={sidebarOpen} setOpen={setSidebarOpen}>
+      <SidebarBody className="justify-between gap-10">
+        <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+          {sidebarOpen ? <Logo /> : <LogoIcon />}
+          <div className="mt-8 flex flex-col gap-2">
+            {links.map((link, idx) => (
+              <SidebarLink key={idx} link={link} />
+            ))}
+          </div>
+        </div>
+        <div>
+          <SidebarLink
+            link={{
+              label: "Manu Arora",
+              href: "#",
+              icon: (
+                <Image
+                  src={seaImage}
+                  className="h-7 w-7 flex-shrink-0 rounded-full"
+                  width={50}
+                  height={50}
+                  alt="Avatar"
+                />
+              ),
+            }}
+          />
+        </div>
+      </SidebarBody>
+    </Sidebar>
+  );
+}
+
+export const Logo = () => {
+  return (
+    <Link
+      href="#"
+      className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
+    >
+      <TentTree className="h-5 w-5 " />
+      <motion.span
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        className="font-medium text-black dark:text-white whitespace-pre"
+      >
+        Travel Dashboard
+      </motion.span>
+    </Link>
+  );
+};
+
+export const LogoIcon = () => {
+  return (
+    <Link
+      href="#"
+      className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
+    >
+      <TentTree className="h-5 w-5 " />
+    </Link>
+  );
+};
+
+export default AdminSidebar;
